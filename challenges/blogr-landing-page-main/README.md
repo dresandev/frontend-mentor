002f28f0b7515ea85cdcd6e18cff5f795bae6532
javier andres solarte yande <javiandres.016@gmail.com>
2022-12-03 01:32:36 -0500

    blogr landing page main

---
+++ challenges/blogr-landing-page-main/README.md
@@ -0,0 +1,41 @@
+# Frontend Mentor - Blogr landing page solution
+
+This is a solution to the [Blogr landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/blogr-landing-page-EX2RLAApP). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Frontend Mentor - Blogr landing page solution](#frontend-mentor---blogr-landing-page-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![desktop-preview](./screenshots/desktop-preview.png)
+![mobile-preview](./screenshots/mobile-preview.png)
+
+### Links
+
+- Solution URL: []()
+- Live Site URL: []()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Mobile-first workflow
---
+++ challenges/blogr-landing-page-main/src/js/app.js
@@ -0,0 +1,68 @@
+const $ = selector => document.querySelector(selector)
+const $$ = selector => document.querySelectorAll(selector)
+
+const hamburgerBtn = $('.menu-btn__icon')
+const menu = $('.nav__menu')
+const navMenuMain = $('.menu__main')
+
+const mainOptions = $$('.main__opt')
+const optDropdown = $$('.opt__dropdown')
+
+const subMenuHeight = []
+
+
+optDropdown.forEach((dropdown) => {
+  subMenuHeight.push(dropdown.clientHeight);
+  dropdown.style.height = 0
+})
+
+
+window.onload = () => {
+
+  hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.classList.toggle('menu-btn__icon--open')
+
+    hamburgerBtn.children[0].classList.toggle('icon__line--top-active')
+    hamburgerBtn.children[1].classList.toggle('icon__line--middle-active')
+    hamburgerBtn.children[2].classList.toggle('icon__line--bottom-active')
+
+    menu.classList.toggle('nav__menu--opened')
+
+    cleanSelectedOptions()
+
+    optDropdown.forEach((dropdown) => {
+      if (dropdown.clientHeight > 0) dropdown.style.height = 0
+    })
+  })
+
+  mainOptions.forEach((option, optionI) => {
+
+    option.addEventListener('click', () => {
+      cleanSelectedOptions()
+
+      optDropdown.forEach((dropdown, dropdownI) => {
+        if (optionI !== dropdownI) dropdown.style.height = 0
+      })
+
+      const dropdownStyle = optDropdown[optionI].style;
+
+      if (dropdownStyle.height === '0px') {
+        dropdownStyle.height = `${subMenuHeight[optionI]}px`
+        option.classList.add('main__opt--active')
+        return
+      }
+
+      dropdownStyle.height = 0
+    })
+
+  })
+
+}
+
+const cleanSelectedOptions = () => {
+  mainOptions.forEach(option => {
+    if (option.classList.contains('main__opt--active')) {
+      option.classList.remove('main__opt--active')
+    }
+  })
+}
